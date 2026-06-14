9abe8b5861d4d12279f5fd82fe6c218b37014b80
Gustavo Alaniz <gus.alaniz81@gmail.com>
2026-06-14 14:44:37 -0300

    Agregamos ejercicios Clase 7 y actualizamos sintaxis JS

---
+++ javaScript/Clase7/Clase7.js
@@ -0,0 +1,13 @@
+let x, y; //Se pueden crear varias variables dentro de una misma línea.
+x = 17, y = 21; // Se puede hacer asignacion de varias variables dentro de la misma líneax
+let z = x + y //Se asigna el valor de la operación
+console.log(z)
+//JAVASCRIPT es keys sensitive, es sensible a mayusculas y minúsculas, se debe escribir bien
+// Una variable no debe comenzar con números porque no lo permite java ni javascript.
+// Tampoco podemos utilizar palabras reservadas del mismo lenguajesa las variables.
+
+let _1num = 31; //No utilizamos números para iniciar el nombre de una variable
+let  rompiendo = "rompe"; //No utilizar palabras reservadas para variables
+
+console.log(_1num)
+console.log(rompiendo)
---
+++ javaScript/Clase7/EjercicioEsMayorDeEdad.js
@@ -0,0 +1,10 @@
+//Ejercicio: Es mayor de edad
+let edad = 14, mayorEdad = 18;
+if (edad >= mayorEdad){
+    console.log("Ested es una persona adulta")
+}
+else{
+    console.log("Ested es una persona menor de edad")
+}
+
+
