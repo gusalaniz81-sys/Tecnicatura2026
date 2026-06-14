3c71c52cab0035409860eabb006ecf100aeb2b25
Gustavo Alaniz <gus.alaniz81@gmail.com>
2026-06-14 10:33:50 -0300

    Clase2 de Javascript y similitudes de sintaix con Java

---
+++ javaScript/Clase5/Case_5TiposDatos.js
@@ -0,0 +1,20 @@
+//Tipo de datos undefined
+var x;
+console.log(x);
+
+x = undefined;
+console.log(typeof x);
+
+//null: Significa ausencia de valor
+var y = null; //null no es un tipo de dato, pero su origen es de tipo object
+console.log(typeof y);
+
+//Tipo de datos array y Empty String
+var autos = ['Citroen','Áudi','BMW','Ford']
+console.log(autos);
+console.log(typeof autos); //Preguntamos que tipo de datos es:
+
+var z = '';
+console.log(z);//Esto se refiere a que es una cadena vacia
+console.log(typeof z);
+
